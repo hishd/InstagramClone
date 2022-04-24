48a1b5bbd431cf68b9f10d352311eb47122e62ba
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-04-24 22:46:31 +0530

    Added Collection View to the Feed Layout

--- InstagramClone/Controller/FeedController.swift
+++ InstagramClone/Controller/FeedController.swift
@@ -8,9 +8,39 @@
 import Foundation
 import UIKit
 
-class FeedController: UIViewController {
+class FeedController: UICollectionViewController {
+    private let reuseIdentifier = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configureUI()
+    }
+}
+
+extension FeedController {
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: UICollectionViewDatasource setup
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: Setting the size of the Collection View Cells
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.height, height: 200)
     }
 }
--- InstagramClone/Controller/MainTabController.swift
+++ InstagramClone/Controller/MainTabController.swift
@@ -17,12 +17,31 @@ class MainTabController: UITabBarController {
     // MARK: Helpers
     
     func configureVC() {
-        let feedVc = FeedController()
-        let searchVc = SearchController()
-        let imageSelectorVc = ImageSelectorController()
-        let notificationVc = NotificationController()
-        let profileVc = ProfileController()
+        let layout = UICollectionViewFlowLayout()
+        let feedVc = createNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        let searchVc = createNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        let imageSelectorVc = createNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        let notificationVc = createNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+        let profileVc = createNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
         
+        /// Setting the baclground of the TabBar since iOS 15 bring transparent Tab Bar
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .systemBackground
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+        }
+        
+        tabBar.tintColor = UIColor(named: "tabBarColor")
         viewControllers = [feedVc, searchVc, imageSelectorVc, notificationVc, profileVc]
+    }
+    
+    func createNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let navVC = UINavigationController(rootViewController: rootViewController)
+        navVC.tabBarItem.image = unselectedImage
+        navVC.tabBarItem.selectedImage = selectedImage
+        navVC.navigationBar.tintColor = .black
+        return navVC
     }
 }
