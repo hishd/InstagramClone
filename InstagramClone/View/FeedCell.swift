bc331e302f572ed9231ca8558880ee365b9d3d2d
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-04-24 23:32:50 +0530

    Added Extensions for the UIViewCOntroller, UIButtom, UIView to set constraints and add functionality

---
+++ InstagramClone/View/FeedCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Hishara Dilshan on 2022-04-24.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
