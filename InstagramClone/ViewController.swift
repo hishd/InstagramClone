0a260ed5e773f03e34ed2f541182f6c10a9dc370
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-04-24 21:14:28 +0530

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Hishara Dilshan on 2022-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
