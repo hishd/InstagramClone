a4d100232f8d3d9784b23d87fbe8fb01bbc014e4
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-04-26 22:34:47 +0530

    Added Authentication View Controllers

---
+++ InstagramClone/Controller/Authentication/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  InstagramClone
+//
+//  Created by Hishara Dilshan on 2022-04-26.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
---
+++ InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationController.swift
+//  InstagramClone
+//
+//  Created by Hishara Dilshan on 2022-04-26.
+//
+
+import Foundation
+import UIKit
+
+
+class RegistrationController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
