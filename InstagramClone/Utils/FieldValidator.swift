4233768cdf9bd5b4f516be88df621d70268827aa
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-05-11 23:35:01 +0530

    Added Validations for full name and user name

--- InstagramClone/Utils/FieldValidator.swift
+++ InstagramClone/Utils/FieldValidator.swift
@@ -13,6 +13,8 @@ class FieldValidator {
     
     lazy var emailPredicate = NSPredicate(format: "SELF MATCHES %@", Patterns.emailRegex.rawValue)
     lazy var passwordPredicate = NSPredicate(format: "SELF MATCHES %@", Patterns.passwordRegex.rawValue)
+    lazy var namePredicate = NSPredicate(format: "SELF MATCHES %@", Patterns.nameRegex.rawValue)
+    lazy var userNamePredicate = NSPredicate(format: "SELF MATCHES %@", Patterns.userNameRegex.rawValue)
     
     func isValidEmailAddress(of email: String) -> Bool {
         return self.emailPredicate.evaluate(with: email)
@@ -22,7 +24,17 @@ class FieldValidator {
         return self.passwordPredicate.evaluate(with: password)
     }
     
+    func isValidPersonName(of name: String) -> Bool {
+        return self.namePredicate.evaluate(with: name)
+    }
+    
+    func isValidUserName(of userName: String) -> Bool {
+        return self.userNamePredicate.evaluate(with: userName)
+    }
+    
     private enum Patterns: String {
+        case userNameRegex = "[A-Za-z]{4,20}"
+        case nameRegex = "[A-Za-z ]{2,100}"
         case emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         case passwordRegex = "[A-Za-z\\d]{6,}"
     }
